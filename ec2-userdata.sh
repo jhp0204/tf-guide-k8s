0b057df1ea9b446e99f68d5be5251ff675f74dd8
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-23 17:46:21 +0900

    Update ec2-userdata.sh

--- ec2-userdata.sh
+++ ec2-userdata.sh
@@ -5,14 +5,14 @@ sleep 120
 apt-get update
 
 DEBIAN_FRONTEND=noninteractive apt-get install resolvconf -yq
-echo "search ${domain}" > /etc/resolvconf/resolv.conf.d/base
+echo "search "${var.env}.lab.io"" > /etc/resolvconf/resolv.conf.d/base
 resolvconf -u
 
 # create ansible user
 groupadd ansible
 useradd -g ansible -m -s /bin/bash ansible
 su - ansible -c "mkdir -m 700 .ssh"
-su - ansible -c "echo \"${ansible_rsa_pub}\" >> /home/ansible/.ssh/authorized_keys"
+su - ansible -c "echo \"${var.ansible_rsa_pub}\" >> /home/ansible/.ssh/authorized_keys"
 su - ansible -c "echo -e \"\\n\" >> /home/ansible/.ssh/authorized_keys"
 su - ansible -c "chmod 600 /home/ansible/.ssh/authorized_keys"
 echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
