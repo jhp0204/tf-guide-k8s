3ab9d12ec003ab3ec71386a4329ea67d1380234c
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-23 17:34:45 +0900

    Update var.tf

--- var.tf
+++ var.tf
@@ -6,16 +6,16 @@ variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
 variable "ansible_rsa_pub" {}
 
-data "template_file" "ec2_userdata" {
-  template = "${file("${path.module}/ec2-userdata.sh")}"
-  variables {
-		AWS_ACCESS_KEY_ID = "${var.AWS_ACCESS_KEY_ID}"
-		AWS_SECRET_ACCESS_KEY = "${var.AWS_SECRET_ACCESS_KEY}"
-		AWS_DEFAULT_REGION = "${var.region}"
-    domain = "${var.env}.lab.io"
-    ansible_rsa_pub = "${var.ansible_rsa_pub}"
-  }
-}
+# data "template_file" "ec2_userdata" {
+#   template = "${file("${path.module}/ec2-userdata.sh")}"
+#   variables {
+# 		AWS_ACCESS_KEY_ID = "${var.AWS_ACCESS_KEY_ID}"
+# 		AWS_SECRET_ACCESS_KEY = "${var.AWS_SECRET_ACCESS_KEY}"
+# 		AWS_DEFAULT_REGION = "${var.region}"
+#     domain = "${var.env}.lab.io"
+#     ansible_rsa_pub = "${var.ansible_rsa_pub}"
+#   }
+# }
 
 variable "ec2_inst" {
 	type = "map"
