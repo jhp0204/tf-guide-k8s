5355ba6a457ede95db90d8d458a9f0cf81138511
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-23 16:54:04 +0900

    Update var.tf

--- var.tf
+++ var.tf
@@ -8,12 +8,12 @@ variable "ansible_rsa_pub" {}
 
 data "template_file" "ec2_userdata" {
   template = "${file("${path.module}/ec2-userdata.sh")}"
-  variables {
-		AWS_ACCESS_KEY_ID = "${var.AWS_ACCESS_KEY_ID}"
-		AWS_SECRET_ACCESS_KEY = "${var.AWS_SECRET_ACCESS_KEY}"
-		AWS_DEFAULT_REGION = "${var.region}"
-    domain = "${var.env}.lab.io"
-    ansible_rsa_pub = "${var.ansible_rsa_pub}"
+  var {
+		'AWS_ACCESS_KEY_ID = "${var.AWS_ACCESS_KEY_ID}"'
+		'AWS_SECRET_ACCESS_KEY = "${var.AWS_SECRET_ACCESS_KEY}"'
+		'AWS_DEFAULT_REGION = "${var.region}"'
+    'domain = "${var.env}.lab.io"'
+    'ansible_rsa_pub = "${var.ansible_rsa_pub}"'
   }
 }
 
